9532357cd6ae42b30ff357da4406c8052048cb72
dmoksedul <dmoksedulislam@gmail.com>
2023-08-19 17:35:53 +0600

    add

--- js/script.js
+++ js/script.js
@@ -4,3 +4,33 @@ document.addEventListener("DOMContentLoaded", function() {
     }, 3000);
   });
   
+let menuToggler = document.getElementById("menu_toggler");
+let mobileMenu = document.getElementById("mobileMenu");
+let togglerIcon = document.querySelector(".togglerIcon");
+
+menuToggler.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+    document.getElementById("togglerbar").classList.toggle("hidden");
+    document.getElementById("togglerx").classList.toggle("hidden");
+}) 
+// scroll to top
+
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// Show the button when the user scrolls down a certain distance
+window.addEventListener("scroll", () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+});
+
+// Scroll to the top smoothly when the button is clicked
+scrollToTopBtn.addEventListener("click", () => {
+  // Use the 'scrollTo' method with behavior set to 'smooth'
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,29 @@ module.exports = {
   content: ['*'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'p-color' : '#31A0FE',
+        's-color' : '#737373',
+        'bg-color' : '#FFFFFF',
+        'txt-color' : '#252B42',
+      },
+    },
+    
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+    },
   },
   plugins: [],
 }
